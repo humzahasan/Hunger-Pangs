5a2ccd138b474cc4e07f6b68c871a2245609ac15
Humza Hasan <humzahasan2101@gmail.com>
2022-03-15 23:40:13 +0530

    feat: added category and hero section for homepage

--- src/pages/Homepage/Herosection/Herosection.jsx
+++ src/pages/Homepage/Herosection/Herosection.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import "./Herosection.css";
+import HeroImage from "../../../assets/pasta.jpg";
 
 const Herosection = () => {
-  return <div>Herosection</div>;
+  return (
+    <>
+      <div class="hero-section">
+        <img class="image" src={HeroImage} alt="food-item" />
+      </div>
+    </>
+  );
 };
 
 export default Herosection;
