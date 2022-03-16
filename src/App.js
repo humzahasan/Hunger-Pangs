12e041356e7dcba445ba7b8991b04037aa59ce7d
Humza Hasan <humzahasan2101@gmail.com>
2022-03-17 01:43:42 +0530

    feat: Added router in project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import { Homepage } from "./pages/Homepage/Homepage";
+import { Wishlist } from "./pages/Wishlist/Wishlist";
 
 function App() {
   return (
     <div className="app">
-      <Homepage />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/wishlist" element={<Wishlist />} />
+      </Routes>
     </div>
   );
 }
