3ed8e2908c9ab71a8f85d20b0eb593a99c6372d0
Humza Hasan <humzahasan2101@gmail.com>
2022-03-26 21:48:56 +0530

    feat: wishlist page added

--- src/reducers/wishlist-reducer.js
+++ src/reducers/wishlist-reducer.js
@@ -1,7 +1,11 @@
 const wishlistReducer = (state, action) => {
   switch (action.type) {
     case "ADD_TO_WISHLIST":
-      if (state.itemInWishList.indexOf(action.payload)) {
+      const itemIndex = state.itemInWishList.findIndex((item) => {
+        return item.name === action.payload.name;
+      });
+
+      if (itemIndex === -1) {
         return {
           ...state,
           itemInWishList: [...state.itemInWishList, action.payload],
