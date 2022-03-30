2dcbc472046fa90ace0f4c081ba1151b40184f04
Humza Hasan <humzahasan2101@gmail.com>
2022-03-30 20:58:29 +0530

    feat: product context added

---
+++ src/context/index-context.js
@@ -0,0 +1,12 @@
+import { CartProvider, useCart } from "./cart-context";
+import { ProductsProvider, useProducts } from "./products-context";
+import { WishlistProvider, useWishlist } from "./wishlist-context";
+
+export {
+  CartProvider,
+  ProductsProvider,
+  WishlistProvider,
+  useCart,
+  useProducts,
+  useWishlist,
+};
