6f63441d20dbd32650119fafd50d32fc68123b80
Humza Hasan <humzahasan2101@gmail.com>
2022-03-30 20:58:46 +0530

    refactor: refactoring components with backend

--- src/pages/Homepage/Trendingitem/Trendingitem.jsx
+++ src/pages/Homepage/Trendingitem/Trendingitem.jsx
@@ -1,71 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useWishlist } from "../../../context/wishlist-context";
 import { useCart } from "../../../context/cart-context";
+
 import "./Trendingitem.css";
-import Pasta from "../../../assets/pasta.jpg";
-import Burger from "../../../assets/burger.jpg";
-import Pizza from "../../../assets/pizza.png";
-import Pancake from "../../../assets/pancake.jpg";
-import Salad from "../../../assets/salad.jpg";
 import { Card } from "../../../components/Card/Card";
+import { useProducts } from "../../../context/products-context";
 
 const Trendingitem = () => {
-  const trendingItemList = [
-    {
-      name: "Pizza",
-      price: 220,
-      productUrl: Pizza,
-      id: 1,
-    },
-    {
-      name: "Burger",
-      price: 110,
-      productUrl: Burger,
-      id: 2,
-    },
-    {
-      name: "Pancake",
-      price: 150,
-      productUrl: Pancake,
-      id: 3,
-    },
-    {
-      name: "Pasta",
-      price: 290,
-      productUrl: Pasta,
-      id: 4,
-    },
-    {
-      name: "Salad",
-      price: 100,
-      productUrl: Salad,
-      id: 5,
-    },
-  ];
+  const [trendingItem, setTrendingItem] = useState();
+
+  const { products } = useProducts();
+  useEffect(() => {
+    setTrendingItem(products?.filter((item) => item.trending).splice(0, 5));
+  }, [products]);
 
   const { dispatch: wishlistDispatch } = useWishlist();
   const { dispatch: cartDispatch } = useCart();
-
+  console.log(trendingItem);
   return (
     <section className="homepage-trending">
       <h2>Trending Items</h2>
       <div className="trending-item">
-        {trendingItemList.map((item) => {
-          return (
-            <Card
-              key={item.id}
-              title={item.name}
-              subtitle={item.price}
-              cardMediaUrl={item.productUrl}
-              addToCart={() =>
-                cartDispatch({ type: "ADD_TO_CART", payload: item })
-              }
-              addToWishlist={() =>
-                wishlistDispatch({ type: "ADD_TO_WISHLIST", payload: item })
-              }
-            />
-          );
-        })}
+        {trendingItem &&
+          trendingItem?.map((item) => {
+            return (
+              <Card
+                key={item.id}
+                badgeContent="Top Seller"
+                title={item.name}
+                subtitle={item.price}
+                cardMediaUrl={item.productUrl}
+                addToCart={() =>
+                  cartDispatch({ type: "ADD_TO_CART", payload: item })
+                }
+                addToWishlist={() =>
+                  wishlistDispatch({ type: "ADD_TO_WISHLIST", payload: item })
+                }
+              />
+            );
+          })}
       </div>
     </section>
   );
