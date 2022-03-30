77ea2d9a82be5ddd740f00ab40e2ea0f3cbec90a
Humza Hasan <humzahasan2101@gmail.com>
2022-03-30 20:57:07 +0530

    feat: added backend mock data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { Cart, Homepage, Wishlist } from "./pages/index-pages";
+import { Products } from "./pages/Products/Products";
 function App() {
   return (
     <div className="app">
@@ -8,6 +9,7 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="/wishlist" element={<Wishlist />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/products" element={<Products />} />
       </Routes>
     </div>
   );
--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -7,33 +7,150 @@ import { v4 as uuid } from "uuid";
 
 export const categories = [
   {
-    _id: uuid(),
-    categoryName: "baskin-robbins",
-    description:
-      "literature in the form of prose, especially novels, that describes imaginary events and people",
+    id: "6d61fe68-6c46-403c-a68f-9c61260d4d41",
+    categoryName: "Recommended",
   },
   {
-    _id: uuid(),
-    categoryName: "biryani",
-    description:
-      "Non-fiction is writing that gives information or describes real events, rather than telling a story.",
+    id: "9d0e54d8-29cf-4cd8-b2de-6cb4cfd833c7",
+    categoryName: "Happyness Gift Pack",
+  },
+  { id: "d652e7b4-50ea-413e-aced-648702c4071c", categoryName: "" },
+  {
+    id: "05171280-c5f0-4500-aa35-a918365edeb7",
+    categoryName: "Ice Cream Sandwich",
   },
   {
-    _id: uuid(),
-    categoryName: "burger-king",
-    description:
-      "Meant to cause discomfort and fear for both the character and readers, horror writers often make use of supernatural and paranormal elements in morbid stories that are sometimes a little too realistic.",
+    id: "ad216a6a-f7a3-4571-9b83-1e9d27ad0a10",
+    categoryName: "Twin combo packs at special prices(10% off)",
   },
   {
-    _id: uuid(),
-    categoryName: "north-indian",
-    description:
-      "Meant to cause discomfort and fear for both the character and readers, horror writers often make use of supernatural and paranormal elements in morbid stories that are sometimes a little too realistic.",
+    id: "a2249d71-d256-46df-bca1-dbfc1f77e446",
+    categoryName: "Breakfast With BK",
   },
   {
-    _id: uuid(),
-    categoryName: "wow-momo",
-    description:
-      "Meant to cause discomfort and fear for both the character and readers, horror writers often make use of supernatural and paranormal elements in morbid stories that are sometimes a little too realistic.",
+    id: "4fbab67b-2f85-4e27-883a-7dcdd19e2ca7",
+    categoryName: "King Deals ( Save Rs 74)",
+  },
+  {
+    id: "a090e28e-7e37-4f30-9942-60a2d2ecd9f8",
+    categoryName: "New Kings Collection Burgers",
+  },
+  { id: "5c6b436e-792b-4fe6-8dea-626b7707b237", categoryName: "Whopper" },
+  {
+    id: "31451a35-7e65-4bc9-b972-f75551f19da7",
+    categoryName: "Meal Combos (Save Rs 45)",
+  },
+  {
+    id: "c302775e-19ea-4a62-803d-b0e8dfb92975",
+    categoryName: "Burgers & Wraps",
+  },
+  {
+    id: "f911a013-a2c5-42ad-88cb-c67e55731aee",
+    categoryName: "What's New",
+  },
+  { id: "e2e5d9a6-137b-42e4-9c9e-e47d4249c2f4", categoryName: "Sides" },
+  { id: "4f6c1504-a100-4c54-9729-2d36cc0f074d", categoryName: "Beverages" },
+  { id: "774f1bea-653f-444c-8230-57870ef1024f", categoryName: "Desserts" },
+  {
+    id: "e2e3711a-de3d-4a1e-91e7-7d0396f428f4",
+    categoryName: "Chicken Wings",
+  },
+  {
+    id: "14d33901-54d9-4b57-8924-09f49d319237",
+    categoryName: "Chilli Momos (New Launch)",
+  },
+  {
+    id: "f7e732dd-1aff-4ede-90b2-9a0b95aa3b13",
+    categoryName: "2022 Feast Up COMBOS",
+  },
+  {
+    id: "dd72a3da-0efa-436d-886e-cfa7bb45fc6a",
+    categoryName: "Steam Momo",
+  },
+  {
+    id: "83b7bcf1-a2f1-46d4-88fa-3225eef8e3e4",
+    categoryName: "Pan Fried Momo",
+  },
+  {
+    id: "345f4a25-7f22-4cd8-b75c-d30eb58d3445",
+    categoryName: "Butter Momo",
+  },
+  {
+    id: "af0112a3-af11-47e0-8d83-f2c52f3df587",
+    categoryName: "Tandoori Momo",
+  },
+  { id: "00ecf375-32a3-4f64-bf0e-021ffbb82103", categoryName: "Thukpas" },
+  {
+    id: "8bcfed3c-a0e5-4f95-aebd-a873a11cea54",
+    categoryName: "Baked Momo Augratin",
+  },
+  { id: "3a0882b9-4df0-4ddb-96bb-669a70a2e6c8", categoryName: "WOK" },
+  { id: "50d8662d-cd6e-40b6-8400-79100b79cad4", categoryName: "Sizzlers" },
+  {
+    id: "dd1ce59c-ef10-40d9-baba-161635de1c4d",
+    categoryName: "Burgers [Momo]",
+  },
+  { id: "72a0486f-da1d-4ec6-8cb2-4e9b30480fd2", categoryName: "Dessert" },
+  { id: "567f0330-de95-402d-af94-eff97c58bbe6", categoryName: "Extra" },
+  { id: "f19bf2f9-2dc6-4403-aa35-95a6f725bf26", categoryName: "Starters" },
+  {
+    id: "f5327c72-0f2e-49f4-9939-4884775c2aa2",
+    categoryName: "Veg Specials",
+  },
+  {
+    id: "3ba7f387-8f4b-4953-a84d-970f5d2f5fd9",
+    categoryName: "Shahi Biryani (serves 1)",
+  },
+  {
+    id: "c23a197d-ae0c-4c4d-89d8-c9c201d89dac",
+    categoryName: "Shaan Biryani (serves 2)",
+  },
+  {
+    id: "23f28b27-cabe-430a-b273-c216560f7106",
+    categoryName: "Alishaan Biryani (serves 4)",
+  },
+  {
+    id: "fb070f37-7147-4af6-a0e3-c24a6325625f",
+    categoryName: "Hyderabad-E-Khaas [Spicy]",
+  },
+  {
+    id: "ac2b6153-1a32-417e-a501-4d4d55222fbe",
+    categoryName: "Dastaan-E-Kebab",
+  },
+  {
+    id: "c1d4ceef-a89b-4639-9619-cc8286cc0153",
+    categoryName: "Bahaar (Extra Meat Biryanis)",
+  },
+  {
+    id: "0629a518-628e-4b62-baad-62a9653cf67b",
+    categoryName: "Biryani Combos",
+  },
+  {
+    id: "b566d506-1398-4cdd-afbf-a3adefbe525c",
+    categoryName: "Make Your Royal Feast",
+  },
+  {
+    id: "363a23ca-dc8d-420c-a38a-a8d5615bd7e2",
+    categoryName: "Mehfil-E-Biryani",
+  },
+  {
+    id: "700c54e2-10dd-404a-9fac-19b80254e22a",
+    categoryName: "Shirin & Sherbet",
+  },
+  {
+    id: "ebe4743c-e925-46bd-b5a7-453710d8f6d4",
+    categoryName: "MONTHEND MOMOMANIA",
+  },
+  {
+    id: "1e073be8-a5c3-43c3-9fcd-786dcc51a017",
+    categoryName: "Match Day Combo [Save Up to 43%]",
+  },
+  {
+    id: "0686e82e-5b24-47b9-aa15-7576ee88ee1b",
+    categoryName: "Fried Momo",
+  },
+  {
+    id: "e3eb7445-bcd1-460b-a307-810fb68b7efd",
+    categoryName: "Match Day Combos",
   },
 ];
