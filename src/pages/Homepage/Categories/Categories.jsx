59e1b26cc214c8f85cf94432bf7d06a1f231dd76
Humza Hasan <humzahasan2101@gmail.com>
2022-04-17 22:02:51 +0530

    fix: reducer modified with filter

--- src/pages/Homepage/Categories/Categories.jsx
+++ src/pages/Homepage/Categories/Categories.jsx
@@ -1,7 +1,9 @@
 import "./Categories.css";
 import { Card } from "../../../components/Card/Card";
+import { useNavigate } from "react-router-dom";
 
 const Categories = () => {
+  const navigate = useNavigate();
   return (
     <article className="catergories">
       <h2>Top Categories</h2>
@@ -11,30 +13,35 @@ const Categories = () => {
             "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/fyoumaj78lmqeg7mmbzn"
           }
           imageOverlay={"ICE CREAM"}
+          clickHandler={() => navigate("/products/ice-cream")}
         />
         <Card
           cardMediaUrl={
             "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/pltx770rrkcghlzx4zwb"
           }
           imageOverlay={"BURGER"}
+          clickHandler={() => navigate("/products/burger-&-wraps")}
         />
         <Card
           cardMediaUrl={
             "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/usgs3ew8xsul9chz8z21"
           }
           imageOverlay={"MOMO"}
+          clickHandler={() => navigate("/products/momo")}
         />
         <Card
           cardMediaUrl={
             "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/rksu3hnjwxwormnosuhr"
           }
           imageOverlay={"BIRYANI"}
+          clickHandler={() => navigate("/products/biryani")}
         />
         <Card
           cardMediaUrl={
             "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/hvf5qxnb7i4llownf0p7"
           }
           imageOverlay={"NORTH INDIAN"}
+          clickHandler={() => navigate("/products/north-indian")}
         />
       </section>
     </article>
